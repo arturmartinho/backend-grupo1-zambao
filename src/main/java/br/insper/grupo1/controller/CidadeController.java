9dae8689b1053a56005b1915f7235ee233e7e1bd
Lucas ikawa <lucasbqi@al.insper.edu.br>
2024-11-05 17:07:41 -0300

    Ta pago

--- src/main/java/br/insper/grupo1/controller/CidadeController.java
+++ src/main/java/br/insper/grupo1/controller/CidadeController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/cidades")
+@CrossOrigin(origins = "http://localhost:3000")
 public class CidadeController {
 
     @Autowired
--- src/main/java/br/insper/grupo1/controller/HotelController.java
+++ src/main/java/br/insper/grupo1/controller/HotelController.java
@@ -6,15 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.insper.grupo1.dto.HotelDTO;
 import br.insper.grupo1.model.Cidade;
@@ -24,6 +16,7 @@ import br.insper.grupo1.service.HotelService;
 
 @RestController
 @RequestMapping("/api/hotels")
+@CrossOrigin(origins = "http://localhost:3000")
 public class HotelController {
 
     @Autowired
